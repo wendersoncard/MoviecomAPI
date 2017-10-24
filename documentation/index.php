63e28192fb8ee7088fd055b5a55cbf28a30619a1
erick <erick@tripecriacao.com.br>
2017-10-24 15:48:24 -0200

    COlocar status de resposta na API e adicionar erros na documentação

--- documentation/index.php
+++ documentation/index.php
@@ -21,7 +21,7 @@
     <section>
       <h2>Token de autenticação</h2>
       <p>
-        O Token de autenticação é provido pela Tripé ao desenvolvedor do sistema. Para obte-lo favor entrar em contato via e-mail com contato@tripecriacao.com.br
+        O Token de autenticação é provido pela Tripé Criação para o desenvolvedor do sistema. Para obte-lo favor entrar em contato via e-mail com contato@tripecriacao.com.br
       </p>
     </section>
 
@@ -30,10 +30,27 @@
 
       <div class="methodUrl">
         <div>GET</div>
-        <div>http://localhost/moviecomAPI/</div>
+        <div>http://moviecom.com.br/moviecomAPI</div>
       </div>
 
       <div class="clearAll"></div>
+
+      <div class="table" style="margin-bottom: 30px;">
+        <div class="tableHeader">
+          <div class="tr">
+            <label>Header</label>
+            <label>Descrição</label>
+            <label class="center">Obrigatório</label>
+          </div>
+        </div>
+        <div class="tableBody">
+          <div class="tr">
+            <label><font>user_token</font></label>
+            <label>Código único fornecido para o desenvolvedor que consumirá os serviços da API.</label>
+            <label class="center">Sim</label>
+          </div>
+        </div>
+      </div>
 
       <div class="table">
         <div class="tableHeader">
@@ -44,11 +61,6 @@
           </div>
         </div>
         <div class="tableBody">
-          <div class="tr">
-            <label><font>token</font></label>
-            <label>Código único fornecido para o desenvolvedor que consumirá os serviços da API.</label>
-            <label class="center">Sim</label>
-          </div>
 
           <div class="tr">
             <label><font>praca</font></label>
@@ -77,14 +89,131 @@
     <section>
       <h2>Exemplo de requisições</h2>
       <div class="code">
-        <span class="comment">//get http://localhost/moviecomAPI/</span>
+        <span class="comment">//GET http://moviecom.com.br/moviecomAPI</span>
         <span class="choma">{</span>
-        <span class="name">token: </span><span class="value">"nrkrA2qSvsCab5oNQdzXviR7ixoLx8Sc",</span>
         <span class="name">praca: </span><span class="value">"TAU",</span>
         <span class="name">data_ini: </span><span class="value">"2017-10-06",</span>
         <span class="name">data_fim: </span><span class="value">"2017-10-07"</span>
         <span class="choma">}</span>
       </div>
+    </section>
+
+    <section>
+      <h2>Exemplo de resposta</h2>
+      <div class="code">
+        <span class="comment">//success</span>
+        <span class="choma">{</span>
+        <span class="name status">"status":</span><span class="value">"Success",</span>
+        <span class="name status">"data": </span><span class="value">[ {</span>
+            <span class="name">"praca": </span><span class="value"> "TAU",</span>
+            <span class="name">"filmes": </span><span class="value"> [</span>
+                <span class="choma sec">{</span>
+                    <span class="name sec">"filme": </span><span class="value"> {</span>
+                        <span class="name thir">"titulo": </span><span class="value"> "Blade Runner 2049",</span>
+                        <span class="name thir">"cartaz": </span><span class="value"> "http://moviecom.com.br/site/wp-content/uploads/2017/08/blade-runner-ok.jpg",</span>
+                        <span class="name thir">"sinopse": </span><span class="value"> "Após trinta anos dos eventos do primeiro filme, a humanidade está ameaçada novamente e o perigo pode ser ainda maior. O novato oficial K (Ryan Gosling), remexe um segredo enterrado há tempos e que tem o potencial de mergulhar a sociedade em completo caos. Essa descoberta leva o policial a uma frenética busca por um ex-blade runner da polícia de Los Angeles, Rick Deckard (Harrison Ford), que está desaparecido há 30 anos.\r\n<div class=\"fichaFilme\"></div>\r\n&nbsp;",</span>
+                        <span class="name thir">"censura": </span><span class="value"> "14",</span>
+                        <span class="name thir">"trailer": </span><span class="value"> "g-LzzkTi6hk",</span>
+                        <span class="name thir">"ticket_filme": </span><span class="value"> "http://moviecom.com.br/vendafilme/?praca=TAU&idfilme=4502",</span>
+                        <span class="name thir">"horarios":  </span><span class="value">[</span>
+                            <span class="choma four">{</span>
+                                <span class="name fif">"sessao": </span><span class="value minor"> {</span>
+                                    <span class="name six">"tipo": </span><span class="value minor"> "DUB",</span>
+                                    <span class="name six">"dia": </span><span class="value minor"> "2017-10-06",</span>
+                                    <span class="name six">"hora": </span><span class="value minor"> "16:15:00",</span>
+                                    <span class="name six">"sala": </span><span class="value minor"> "03",</span>
+                                    <span class="name six">"ticket_sessao": </span><span class="value minor"> "http://moviecom.com.br/vendasessao/?idprog=11e7a87469b29cc480ce84349711a651&praca=TAU"</span>
+                                <span class="choma fif">}</span>
+                            <span class="choma four">},</span>
+                            <span class="choma four">{</span>
+                                <span class="name fif">"sessao": </span><span class="value minor"> {</span>
+                                    <span class="name six">"tipo": </span><span class="value minor"> "DUB",</span>
+                                    <span class="name six">"dia": </span><span class="value minor"> "2017-10-06",</span>
+                                    <span class="name six">"hora": </span><span class="value minor"> "19:30:00",</span>
+                                    <span class="name six">"sala": </span><span class="value minor"> "03",</span>
+                                    <span class="name six">"ticket_sessao": </span><span class="value minor"> "http://moviecom.com.br/vendasessao/?idprog=11e7a87469b29d3480ce84349711a651&praca=TAU"</span>
+                                <span class="choma fif">}</span>
+                            <span class="choma four">},</span>
+                            <span class="choma four">{</span>
+                                <span class="name fif">"sessao": </span><span class="value minor"> {</span>
+                                    <span class="name six">"tipo": </span><span class="value minor"> "LEG 3D",</span>
+                                    <span class="name six">"dia": </span><span class="value minor"> "2017-10-06",</span>
+                                    <span class="name six">"hora": </span><span class="value minor"> "21:10:01",</span>
+                                    <span class="name six">"sala": </span><span class="value minor"> "04",</span>
+                                    <span class="name six">"ticket_sessao": </span><span class="value minor"> "http://moviecom.com.br/vendasessao/?idprog=11e7a87469b29d5080ce84349711a651&praca=TAU"</span>
+                                <span class="choma fif">}</span>
+                            <span class="choma four">}</span>
+                        <span class="choma thir">]</span>
+                    <span class="choma sec">}</span>
+                <span class="choma name">}</span>
+            <span class="choma name">]</span>
+            <span class="choma status">} ]</span>
+        <span class="choma">}</span>
+      </div>
+    </section>
+    <section>
+      <h2>Erros</h2>
+
+      <p>Quando não for enviado um dos três parâmetors obrigatórios, a API retornará o erro a seguir:</p>
+      <div class="code">
+        <span class="comment">//error</span>
+        <span class="choma">{</span>
+        <span class="name status">"status":</span><span class="value">"Error",</span>
+        <span class="name status">"data": </span><span class="value">[</span>
+        <span class="name sec">"É necessário enviar os parâmetros de data início, data fim e código da praça."</span>
+        <span class="choma status">]</span>
+        <span class="choma">}</span>
+      </div>
+
+      <p>Quando não for enviado o <strong>token de autenticação</strong> (ou estiver em branco), a API retornará o erro a seguir:</p>
+      <div class="code">
+        <span class="comment">//error</span>
+        <span class="choma">{</span>
+        <span class="name status">"status":</span><span class="value">"Error",</span>
+        <span class="name status">"data": </span><span class="value">[</span>
+        <span class="name sec">"É necessário enviar o token de autenticação."</span>
+        <span class="choma status">]</span>
+        <span class="choma">}</span>
+      </div>
+
+      <p>Quando o <strong>token de autenticação</strong> tiver sido enviado mas não foi localizado pelo sistema, a API detornará o erro a seguir:</p>
+      <div class="code">
+        <span class="comment">//error</span>
+        <span class="choma">{</span>
+        <span class="name status">"status":</span><span class="value">"Error",</span>
+        <span class="name status">"data": </span><span class="value">[</span>
+        <span class="name sec">"Token de autenticação inválido."</span>
+        <span class="choma status">]</span>
+        <span class="choma">}</span>
+      </div>
+
+      <p>Quando as <strong>datas de início e fim</strong> tiverem uma diferença maior do que 2 meses (60 dias), a API retornará o erro a seguir:</p>
+      <div class="code">
+        <span class="comment">//error</span>
+        <span class="choma">{</span>
+        <span class="name status">"status":</span><span class="value">"Error",</span>
+        <span class="name status">"data": </span><span class="value">[</span>
+        <span class="name sec">"Número máximo de dias disponíveis para consulta é de 60 dias."</span>
+        <span class="choma status">]</span>
+        <span class="choma">}</span>
+      </div>
+
+      <p>Caso o sistema da API estiver com algum erro interno temporariamente, a seguinte mensagem será retornada:</p>
+      <div class="code">
+        <span class="comment">//error</span>
+        <span class="choma">{</span>
+        <span class="name status">"status":</span><span class="value">"Error",</span>
+        <span class="name status">"data": </span><span class="value">[</span>
+        <span class="name sec">"Erro interno."</span>
+        <span class="choma status">]</span>
+        <span class="choma">}</span>
+      </div>
+
+    </section>
+
+    <section>
+      <h2>Exemplo de requisição via Jquery Ajax:</h2>
+      <img src="exemplo.png" style="width: 100%;">
     </section>
   </div>
 </body>
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $db_id    = "moviecom";
 $db_pass  = "cumbaca@#";
 
 $cn = mysqli_connect($db_host, $db_id, $db_pass, $db_name);
-
+/* functions */
 function anti_injection($sql){
    // remove palavras que contenham sintaxe sql
    $sql = str_replace("from", "", $sql);
@@ -35,135 +35,226 @@ function anti_injection($sql){
    return $sql;
 }
 
+function verificaRegistraTokenUsuario($cn, $token){
+  $token = anti_injection($token);
+
+  $query = mysqli_query($cn, "SELECT * FROM api_user_token where token = '$token'");
+
+  $usuario = mysqli_fetch_row($query)[1];
+
+  if($usuario == ""){
+    return false;
+  }
+  return true;
+}
+
+function registraLogRequisicao($cn, $token, $praca, $data_inicio, $data_fim, $status){
+  //usuario
+  $query = mysqli_query($cn, "SELECT * FROM api_user_token where token = '$token'");
+  $usuario = mysqli_fetch_row($query)[1];
+
+  //date_req
+  $data = date("Y-m-d H:i:s");
+
+  //params
+  $params = "Praca: ".$praca." DataIni: ".$data_inicio." DataFim: ".$data_fim." Token: ".$token;
+
+  $query = mysqli_query($cn, "INSERT INTO api_user_token_req (user_req, date_req, params_req, status_req, praca_req) VALUES ('$usuario', '$data', '$params', '$status', '$praca')");
+}
+
+function retornaJson($programacao){
+  print(json_encode($programacao, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
+}
+
+/*fim functions*/
+
 header('Content-Type: application/json; charset=utf-8');
 
-if
-(
-   (!isset($_GET['praca']) || trim($_GET['praca']) == "") || 
-   (!isset($_GET['data_fim']) || trim($_GET['data_fim']) == "") || 
-   (!isset($_GET['data_fim']) || trim($_GET['data_ini']) == "") 
+$programacao = array();
+$programacao['status'] = "";
+$programacao['data'] = array();
 
-){
-   echo 'É necessário enviar os parâmetros de data início, data fim e código da praça';
+//parâmetros válidos
+if((!isset($_GET['praca']) || trim($_GET['praca']) == "") || (!isset($_GET['data_fim']) || trim($_GET['data_fim']) == "") || (!isset($_GET['data_ini']) || trim($_GET['data_ini']) == "") ){
+  $programacao['status'] = "Error";
+  $programacao['data'][] = "É necessário enviar os parâmetros de data início, data fim e código da praça.";
+
+  registraLogRequisicao($cn, "", "", "", "", "Error. Invalid params");
+
+  retornaJson($programacao);
+  return false;
+}
+//token no get
+else if(!isset(getallheaders()['user_token']) || trim(getallheaders()['user_token']) == ""){
+  $programacao['status'] = "Error";
+  $programacao['data'][] = "É necessário enviar o token de autenticação.";
+
+  registraLogRequisicao($cn, "", anti_injection($_GET['praca']), anti_injection($_GET['data_ini']), anti_injection($_GET['data_fim']), "Error. No token");
+
+  retornaJson($programacao);
+  return false;
+}
+//autenticando
+else if(!verificaRegistraTokenUsuario($cn, getallheaders()['user_token'])){
+  $programacao['status'] = "Error";
+  $programacao['data'][] = "Token de autenticação inválido.";
+
+  registraLogRequisicao($cn, anti_injection(getallheaders()['user_token']), anti_injection($_GET['praca']), anti_injection($_GET['data_ini']), anti_injection($_GET['data_fim']), "Error. Invalid token");
+
+  retornaJson($programacao);
+  return false;
 }
 else{
-   $praca = anti_injection($_GET['praca']);
-   $dataIni = anti_injection($_GET['data_ini']);
-   $dataFim = anti_injection($_GET['data_fim']);
+    $praca = anti_injection($_GET['praca']);
+    $dataIni = anti_injection($_GET['data_ini']);
+    $dataFim = anti_injection($_GET['data_fim']);
+    $token = anti_injection(getallheaders()['user_token']);
 
-   $d1 = strtotime($dataIni);
-   $d2 = strtotime($dataFim);
+    $d1 = strtotime($dataIni);
+    $d2 = strtotime($dataFim);
 
-   $dias_diferenca = ((($d2 - $d1)/60)/60)/24;
-   if($dias_diferenca > 60){
-      echo 'Número máximo de dias disponíveis para consulta é de 60 dias.';
-      return false;
-   } 
+    $dias_diferenca = ((($d2 - $d1)/60)/60)/24;
+    if($dias_diferenca > 60){
+        $programacao['status'] = "Error";
+        $programacao['data'][] = "Número máximo de dias disponíveis para consulta é de 60 dias."; 
 
-   mysqli_query($cn, 'SET CHARACTER SET utf8');
+        registraLogRequisicao($cn, $token, $praca, $dataIni, $dataFim, "Error. 60 days");
 
-   $query_filmes = "SELECT distinct(IDFILMEPAI_DESC) FROM sys_programacao WHERE CODPRACA = '$praca' AND DATASESSAO > '$dataIni' AND DATASESSAO < '$dataFim' ORDER BY IDFILMEPAI_DESC";
-   $result_filmes = mysqli_query($cn, $query_filmes);
+        retornaJson($programacao);
+        return false;
+    }
+    try{ //inicio try
+    
+      mysqli_query($cn, 'SET CHARACTER SET utf8');
 
-   $programacao = array();
-   $programacao['praca'] = $praca; 
+      $query_filmes = "SELECT distinct(IDFILMEPAI_DESC) FROM sys_programacao WHERE CODPRACA = '$praca' AND DATASESSAO > '$dataIni' AND DATASESSAO < '$dataFim' ORDER BY IDFILMEPAI_DESC";
+      $result_filmes = mysqli_query($cn, $query_filmes);
 
-   while($row = mysqli_fetch_assoc($result_filmes)){
-
-      $nome_filme = $row['IDFILMEPAI_DESC'];
-
-      $query = "SELECT * FROM sys_programacao WHERE CODPRACA = '$praca' AND DATASESSAO > '$dataIni 00:00:00' AND DATASESSAO < '$dataFim 00:00:00' AND IDFILMEPAI_DESC = '$nome_filme' ORDER BY IDFILMEPAI_DESC, DATASESSAO";
-
-      $result = mysqli_query($cn, $query);
-
-      //sessoes
-      $horarios = array();
-
-      $codigo_csn = "";
-
-      while($row1 = mysqli_fetch_assoc($result)){
-
-         //tipo
-         $tipo = "";
-         $tipoCod = $row1['DUBLADO'].$row1['LEGENDADO'].$row1['TRESD']; //bool dub leg 3d
-
-         if ($tipoCod == "100"){
-               $tipo = 'DUB';
-         }
-         else if ($tipoCod == "010"){
-            $tipo = 'LEG';
-         }
-         else if ($tipoCod == "101"){
-            $tipo = 'DUB 3D';
-         }
-         else if ($tipoCod == "011"){
-            $tipo = 'LEG 3D';
-         }
-         else if ($tipoCod == "000"){
-            $tipo = 'NACIONAL';
-         }
-
-         //dia / hora
-         $data = split(' ', $row1['DATASESSAO']);
-         $dia = $data[0];
-         $hora = $data[1];
-
-
-         $horarios[] = array(
-             'sessao' => array(
-                    'tipo' =>  $tipo,
-                    'dia' => $dia,
-                    'hora' => $hora,
-                    'sala' => substr($row1['CODSALA'], 4),
-                    'ticket_sessao' => 'http://moviecom.com.br/vendasessao/?idprog='.$row1['IDPROG'].'&praca='.$praca
-                 )
-         );
-
-         //codigo csn
-         $codigo_csn = $row1['IDFILMEPAI'];
+      if(!$result_filmes){
+        throw new Exception("Erro interno.");
       }
-      //fim sessoes
 
-      
-      //get post id
-      $post_id_query = mysqli_query($cn, "SELECT * FROM wp_postmeta where meta_value = $codigo_csn and meta_key = 'codigo_csn'");
-      $post_id = mysqli_fetch_row($post_id_query)[1];
+      $programacao['data'][]['praca'] = $praca; 
 
-      //get post
-      $post = mysqli_query($cn, "SELECT * FROM wp_posts where id = $post_id");
+      while($row = mysqli_fetch_assoc($result_filmes)){ //while filmes
+        $nome_filme = $row['IDFILMEPAI_DESC'];
 
-      //get sinopse
-      $sinopse = mysqli_fetch_row($post)[4];
+        $query = "SELECT * FROM sys_programacao WHERE CODPRACA = '$praca' AND DATASESSAO > '$dataIni 00:00:00' AND DATASESSAO < '$dataFim 00:00:00' AND IDFILMEPAI_DESC = '$nome_filme' ORDER BY IDFILMEPAI_DESC, DATASESSAO";
 
-      //get cartaz
-      $query_cartaz = mysqli_query($cn, "SELECT * FROM wp_posts WHERE post_parent = '$post_id' AND post_type = 'attachment'");
-      $cartaz = mysqli_fetch_row($query_cartaz)[18];
+        $result = mysqli_query($cn, $query);
 
-      //get censura
-      $query_censura = mysqli_query($cn, "SELECT * FROM wp_postmeta where meta_key = 'censura' and post_id = '$post_id'");
-      $censura = mysqli_fetch_row($query_censura)[3];
+        if(!$result){
+          throw new Exception("Erro interno.");
+        }
 
-      //get trailer
-      $query_trailer = mysqli_query($cn, "SELECT * FROM wp_postmeta where meta_key = 'trailer' and post_id = '$post_id'");
-      $trailer = mysqli_fetch_row($query_trailer)[3];
+        //sessoes
+        $horarios = array();
+        $codigo_csn = "";
 
-      //cria json final do filme
-      $programacao['filmes'][] = array (
-           'filme' => array(
-           'titulo' => $row['IDFILMEPAI_DESC'],
-           'cartaz' => $cartaz,
-           'sinopse' => $sinopse,
-           'censura' => $censura,
-           'trailer' => $trailer,
-           'ticket_filme' => 'http://moviecom.com.br/vendafilme/?praca='.$praca.'&idfilme='.$codigo_csn,
-           'horarios' => $horarios,
-           )
-      );
-   }
+        while($row1 = mysqli_fetch_assoc($result)){
+          //tipo
+          $tipo = "";
+          $tipoCod = $row1['DUBLADO'].$row1['LEGENDADO'].$row1['TRESD']; //bool dub leg 3d
+
+          if ($tipoCod == "100"){
+              $tipo = 'DUB';
+          }
+          else if ($tipoCod == "010"){
+              $tipo = 'LEG';
+          }
+          else if ($tipoCod == "101"){
+              $tipo = 'DUB 3D';
+          }
+          else if ($tipoCod == "011"){
+              $tipo = 'LEG 3D';
+          }
+          else if ($tipoCod == "000"){
+              $tipo = 'NACIONAL';
+          }
+
+          //dia / hora
+          $data = split(' ', $row1['DATASESSAO']);
+          $dia = $data[0];
+          $hora = $data[1];
 
 
-   print(json_encode($programacao, JSON_PRETTY_PRINT));
-   print(json_encode($programacao, JSON_UNESCAPED_UNICODE)); //pros caracter especial funcionar
+          $horarios[] = array(
+              'sessao' => array(
+                  'tipo' =>  $tipo,
+                  'dia' => $dia,
+                  'hora' => $hora,
+                  'sala' => substr($row1['CODSALA'], 4),
+                  'ticket_sessao' => stripslashes('http://moviecom.com.br/vendasessao/?idprog='.$row1['IDPROG'].'&praca='.$praca)
+              )
+          );
+           //codigo csn
+           $codigo_csn = $row1['IDFILMEPAI'];
+        } //fim while sessoes
+        //fim sessoes
+
+        //get post id
+        $post_id_query = mysqli_query($cn, "SELECT * FROM wp_postmeta where meta_value = $codigo_csn and meta_key = 'codigo_csn'");
+        if(!$post_id_query){
+          throw new Exception("Erro interno.");
+        }
+        $post_id = mysqli_fetch_row($post_id_query)[1];
+
+        //get post
+        $post = mysqli_query($cn, "SELECT * FROM wp_posts where id = $post_id");
+        if(!$post){
+          throw new Exception("Erro interno.");
+        }
+        //get sinopse
+        $sinopse = mysqli_fetch_row($post)[4];
+
+        //get cartaz
+        $query_cartaz = mysqli_query($cn, "SELECT * FROM wp_posts WHERE post_parent = '$post_id' AND post_type = 'attachment'");
+        if(!$query_cartaz){
+          throw new Exception("Erro interno.");
+        }
+        $cartaz = mysqli_fetch_row($query_cartaz)[18];
+
+        //get censura
+        $query_censura = mysqli_query($cn, "SELECT * FROM wp_postmeta where meta_key = 'censura' and post_id = '$post_id'");
+        if(!$query_censura){
+          throw new Exception("Erro interno.");
+        }
+        $censura = mysqli_fetch_row($query_censura)[3];
+
+        //get trailer
+        $query_trailer = mysqli_query($cn, "SELECT * FROM wp_postmeta where meta_key = 'trailer' and post_id = '$post_id'");
+        if(!$query_trailer){
+          throw new Exception("Erro interno.");
+        }
+        $trailer = mysqli_fetch_row($query_trailer)[3];
+
+        //cria json final do filme
+        $programacao['data'][0]['filmes'][] = array (
+               'filme' => array(
+               'titulo' => $row['IDFILMEPAI_DESC'],
+               'cartaz' => $cartaz,
+               'sinopse' => $sinopse,
+               'censura' => $censura,
+               'trailer' => $trailer,
+               'ticket_filme' => 'http://moviecom.com.br/vendafilme/?praca='.$praca.'&idfilme='.$codigo_csn,
+               'horarios' => $horarios,
+               )
+        );
+      } //fim while filmes
+      $programacao['status'] = "Success";
+
+      registraLogRequisicao($cn, $token, $praca, $dataIni, $dataFim, "Success");
+
+      retornaJson($programacao);
+    } //fim try
+    catch(Exception $ex){
+      $programacao['status'] = "Error";
+      $programacao['data'][] = $ex->getMessage(); 
+
+      registraLogRequisicao($cn, $token, $praca, $dataIni, $dataFim, $ex->getMessage());
+
+      retornaJson($programacao);
+      return false;
+    }
 }
-
 ?>
